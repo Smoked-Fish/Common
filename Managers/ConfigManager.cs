2f86706b9e47eb992aefba538a8606bfe6643c16
Smoked Fish <espy.jacob@gmail.com>
2024-05-09 04:56:21 -0700

    custom button and separator options

--- Managers/ConfigManager.cs
+++ Managers/ConfigManager.cs
@@ -1,4 +1,8 @@
-﻿using Common.Interfaces;
+﻿#nullable enable
+using Common.Interfaces;
+using HarmonyLib;
+using SplitScreenRegions.Framework.Patches;
+using SplitScreenRegions.Framework.UI;
 using StardewModdingAPI;
 using StardewModdingAPI.Utilities;
 using System;
@@ -8,7 +12,7 @@ namespace Common.Managers
 {
     public static class TranslationHelper
     {
-        private static ITranslationHelper _translations;
+        private static ITranslationHelper? _translations;
 
         public static void Init(ITranslationHelper translations)
         {
@@ -17,47 +21,62 @@ namespace Common.Managers
 
         public static Translation GetByKey(string key)
         {
+            if (_translations == null)
+            {
+                throw new InvalidOperationException("Translations collection is not initialized.");
+            }
             return _translations.Get(key);
         }
     }
 
     public static class ConfigManager
     {
-        private static IManifest _manifest;
-        private static object _config;
-        private static IMonitor _monitor;
-        public static IGenericModConfigMenuApi _configApi;
-        private static ApiManager _apiManager;
-        public static void Initialize(IManifest manifest, object config, IModHelper helper, IMonitor monitor)
+        // Fields
+        private static IManifest? _manifest;
+        private static object? _config;
+        private static IMonitor? _monitor;
+        private static IGenericModConfigMenuApi? _configApi;
+        public static void Initialize(IManifest manifest, object config, IModHelper helper, IMonitor monitor, Harmony harmony)
         {
             _manifest = manifest ?? throw new ArgumentNullException(nameof(manifest));
             _config = config ?? throw new ArgumentNullException(nameof(config));
-            _monitor = monitor;
-            _apiManager = new ApiManager(helper, monitor);
-            _configApi = _apiManager.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu", false);
-            _configApi.Register(manifest, resetAction, () => helper.WriteConfig(_config));
+            Monitor = monitor;
+            ApiManager apiManager = new(helper, monitor);
+            ConfigApi = apiManager.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu", false);
+            ConfigApi?.Register(manifest, ResetAction, () => helper.WriteConfig(_config));
+
+            if (ConfigApi != null)
+            {
+                new PageHelper(harmony, monitor).Apply();
+                new TooltipHelper(harmony, monitor).Apply();
+            }
 
             TranslationHelper.Init(helper.Translation);
         }
 
+        // Properties
+        public static Action ResetAction => resetAction;
+        public static IGenericModConfigMenuApi? ConfigApi { get => _configApi; set => _configApi = value; }
+        public static IMonitor? Monitor { get => _monitor; set => _monitor = value; }
+
         public static void AddOption(string name)
         {
-            if (_configApi == null)
+            if (ConfigApi == null)
             {
-                _monitor.LogOnce($"GenericModConfigMenu not found.", LogLevel.Debug);
+                Monitor?.LogOnce($"GenericModConfigMenu not found.", LogLevel.Debug);
                 return;
             }
 
             if (_config == null)
             {
-                _monitor.Log($"Error: ModConfig is not initialized.", LogLevel.Error);
+                Monitor?.Log($"Error: ModConfig is not initialized.", LogLevel.Error);
                 return;
             }
 
-            PropertyInfo propertyInfo = _config.GetType().GetProperty(name);
+            PropertyInfo? propertyInfo = _config.GetType().GetProperty(name);
             if (propertyInfo == null)
             {
-                _monitor.Log($"Error: Property '{name}' not found in configuration object.", LogLevel.Error);
+                Monitor?.Log($"Error: Property '{name}' not found in configuration object.", LogLevel.Error);
                 return;
             }
 
@@ -66,7 +85,7 @@ namespace Common.Managers
 
             if (getName == null || getDescription == null)
             {
-                _monitor.Log($"Error: Localization keys for '{name}' not found.", LogLevel.Error);
+                Monitor?.Log($"Error: Localization keys for '{name}' not found.", LogLevel.Error);
                 return;
             }
 
@@ -75,7 +94,7 @@ namespace Common.Managers
 
             if (getterMethod == null || setterMethod == null)
             {
-                _monitor.Log($"Error: The get/set methods are null for property '{name}'.", LogLevel.Error);
+                Monitor?.Log($"Error: The get/set methods are null for property '{name}'.", LogLevel.Error);
                 return;
             }
 
@@ -85,32 +104,58 @@ namespace Common.Managers
             switch (propertyInfo.PropertyType.Name)
             {
                 case nameof(Boolean):
-                    _configApi.AddBoolOption(_manifest, (Func<bool>)getter, (Action<bool>)setter, getName, getDescription);
+                    ConfigApi.AddBoolOption(_manifest, (Func<bool>)getter, (Action<bool>)setter, getName, getDescription);
                     break;
                 case nameof(Int32):
-                    _configApi.AddNumberOption(_manifest, (Func<int>)getter, (Action<int>)setter, getName, getDescription);
+                    ConfigApi.AddNumberOption(_manifest, (Func<int>)getter, (Action<int>)setter, getName, getDescription);
                     break;
                 case nameof(Single):
-                    _configApi.AddNumberOption(_manifest, (Func<float>)getter, (Action<float>)setter, getName, getDescription);
+                    ConfigApi.AddNumberOption(_manifest, (Func<float>)getter, (Action<float>)setter, getName, getDescription);
                     break;
                 case nameof(String):
-                    _configApi.AddTextOption(_manifest, (Func<string>)getter, (Action<string>)setter, getName, getDescription);
+                    ConfigApi.AddTextOption(_manifest, (Func<string>)getter, (Action<string>)setter, getName, getDescription);
                     break;
                 case nameof(SButton):
-                    _configApi.AddKeybind(_manifest, (Func<SButton>)getter, (Action<SButton>)setter, getName, getDescription);
+                    ConfigApi.AddKeybind(_manifest, (Func<SButton>)getter, (Action<SButton>)setter, getName, getDescription);
                     break;
                 case nameof(KeybindList):
-                    _configApi.AddKeybindList(_manifest, (Func<KeybindList>)getter, (Action<KeybindList>)setter, getName, getDescription);
+                    ConfigApi.AddKeybindList(_manifest, (Func<KeybindList>)getter, (Action<KeybindList>)setter, getName, getDescription);
                     break;
                 default:
-                    _monitor.Log($"Error: Unsupported property type '{propertyInfo.PropertyType.Name}' for '{name}'.", LogLevel.Error);
+                    Monitor?.Log($"Error: Unsupported property type '{propertyInfo.PropertyType.Name}' for '{name}'.", LogLevel.Error);
                     break;
             }
         }
 
-        static Action resetAction = () =>
+        public static void AddButtonOption(IManifest mod, Func<string> leftText, Func<string> rightText, string? fieldId = null, bool rightHover = false, bool leftHover = false, Func<string>? hoverText = null)
         {
-            MethodInfo resetMethod = _config.GetType().GetMethod("InitializeDefaultConfig", BindingFlags.Public | BindingFlags.Instance);
+            if (ConfigApi == null) return;
+
+            var buttonOption = new ButtonOptions(leftText: leftText(), rightText: rightText(), fieldID: fieldId, rightHover: rightHover, leftHover: leftHover, hoverText != null ? hoverText() : null);
+
+            ConfigApi.AddComplexOption(
+                mod: mod,
+                name: () => "",
+                draw: (batch, position) => buttonOption.Draw(batch, (int)position.X, (int)position.Y),
+                height: () => buttonOption.RightTextHeight,
+                beforeMenuOpened: () => { },
+                beforeSave: () => { },
+                afterReset: () => { },
+                fieldId: fieldId
+            );
+        }
+
+        public static void AddHorizontalSeparator(IManifest mod)
+        {
+            if (ConfigApi == null) return;
+
+            var separatorOption = new SeparatorOptions();
+            ConfigApi.AddComplexOption(mod: mod, name: () => "", draw: SeparatorOptions.Draw);
+        }
+
+        private static readonly Action resetAction = () =>
+        {
+            MethodInfo? resetMethod = _config?.GetType().GetMethod("InitializeDefaultConfig", BindingFlags.Public | BindingFlags.Instance);
             if (resetMethod != null)
             {
                 // Invoke ResetConfig method
@@ -118,7 +163,7 @@ namespace Common.Managers
             }
             else
             {
-                _monitor.Log("Error: ResetConfig method not found.", LogLevel.Error);
+                Monitor?.Log("Error: ResetConfig method not found.", LogLevel.Error);
             }
         };
     }
