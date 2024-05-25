7a0ca9a68364580336654a634cd56b4d3f5864b7
Smoked Fish <espy.jacob@gmail.com>
2024-05-24 17:40:56 -0700

    dummy IConfigurable

--- Interfaces/IConfigurable.cs
+++ Interfaces/IConfigurable.cs
@@ -1,14 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Common.Interfaces
+﻿namespace Common.Interfaces
 {
     public interface IConfigurable
     {
-        void SetConfig(string propertyName, object value);
-        void InitializeDefaultConfig(string category = null);
     }
 }
