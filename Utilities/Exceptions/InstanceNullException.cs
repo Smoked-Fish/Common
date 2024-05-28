93cce880bbe1d6b783618a30249351635b9a5936
Smoked Fish <espy.jacob@gmail.com>
2024-05-28 04:32:37 -0700

    custom exceptions

---
+++ Utilities/Exceptions/InstanceNullException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Common.Utilities.Exceptions
+{
+    public class InstanceNullException : Exception
+    {
+        public InstanceNullException() { }
+        public InstanceNullException(string message) : base(message) { }
+        public InstanceNullException(string? message, Exception? innerException) : base(message, innerException) { }
+    }
+}
---
+++ Utilities/Exceptions/PatchingException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Common.Utilities.Exceptions
+{
+    public class PatchingException : Exception
+    {
+        public PatchingException()
+        {
+        }
+
+        public PatchingException(string message) : base(message)
+        {
+        }
+
+        public PatchingException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public PatchingException(string originalMethod, string newMethod, Exception innerException)
+            : this($"An error occurred while patching {originalMethod} with {newMethod}. See inner exception for details.", innerException)
+        {
+            OriginalMethod = originalMethod;
+            NewMethod = newMethod;
+        }
+
+        public string? OriginalMethod { get; }
+        public string? NewMethod { get; }
+    }
+}
