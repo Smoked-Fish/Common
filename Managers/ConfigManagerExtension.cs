62f5f9abdefe81816a45e18a6365f2832a4c672a
Smoked Fish <espy.jacob@gmail.com>
2024-05-28 23:57:13 -0700

    Renamed for clearity and partial class has its own Initialize that enables harmony

--- Managers/ConfigManagerExtension.cs
+++ Managers/ConfigManagerExtension.cs
@@ -1,14 +1,21 @@
-﻿#if EnableCommonPatches
-#nullable enable
-using HarmonyLib;
-using Common.Utilities.Options;
+﻿#nullable enable
 using Common.Helpers;
+using Common.Interfaces;
+using Common.Utilities;
+using Common.Utilities.Options;
+using StardewModdingAPI;
 using System;
 
 namespace Common.Managers
 {
     public static partial class ConfigManager
     {
+        public static void Initialize(IManifest manifest, IConfigurable config, IModHelper helper, IMonitor monitor, bool enablePatches)
+        {
+            Initialize(manifest, config, helper, monitor);
+            EnablePatches();
+        }
+
         public static void AddButtonOption(
             string leftText, string? rightText = null, string? descText = null, string? hoverText = null,
             bool renderLeft = false, bool renderRight = false, string? fieldId = null, Action? afterReset = null)
@@ -19,12 +26,7 @@ namespace Common.Managers
             descText ??= leftText;
             hoverText ??= leftText;
 
-            string leftTextLocalized() => I18n.GetByKey($"Config.{ModNamespace}.{leftText}.Title");
-            string rightTextLocalized() => I18n.GetByKey($"Config.{ModNamespace}.{rightText}.Button");
-            string descTextLocalized() => I18n.GetByKey($"Config.{ModNamespace}.{descText}.Description");
-            string hoverTextLocalized() => I18n.GetByKey($"Config.{ModNamespace}.{hoverText}.Hover");
-
-            var buttonOption = new ButtonOptions(leftTextLocalized, rightTextLocalized, descTextLocalized, hoverTextLocalized, renderLeft, renderRight, fieldId);
+            var buttonOption = new ButtonOptions(LeftTextLocalized, RightTextLocalized, DescTextLocalized, HoverTextLocalized, renderLeft, renderRight, fieldId);
 
             ConfigApi!.AddComplexOption(
                 mod: Manifest!,
@@ -35,6 +37,12 @@ namespace Common.Managers
                 afterReset: afterReset,
                 height: () => buttonOption.RightTextHeight,
                 fieldId: fieldId);
+            return;
+
+            string LeftTextLocalized() => I18n.GetByKey($"Config.{ModNamespace}.{leftText}.Title");
+            string RightTextLocalized() => I18n.GetByKey($"Config.{ModNamespace}.{rightText}.Button");
+            string DescTextLocalized() => I18n.GetByKey($"Config.{ModNamespace}.{descText}.Description");
+            string HoverTextLocalized() => I18n.GetByKey($"Config.{ModNamespace}.{hoverText}.Hover");
         }
 
         public static void AddHorizontalSeparator()
@@ -47,14 +55,11 @@ namespace Common.Managers
                 draw: SeparatorOptions.Draw);
         }
 
-        private static void EnablePatches(object? harmony)
+        private static void EnablePatches()
         {
-            if (ConfigApi != null && harmony is Harmony realHarmony)
-            {
-                new PageHelper(realHarmony).Apply();
-                new TooltipHelper(realHarmony).Apply();
-            }
+            if (ConfigApi == null) return;
+            new PageHelper().Apply();
+            new TooltipHelper().Apply();
         }
     }
 }
-#endif
