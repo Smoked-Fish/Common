9576b5e2bc7328b60b69b73eefaf1241bc0527fe
Smoked Fish <espy.jacob@gmail.com>
2024-05-29 12:25:33 -0700

    made ApiRegistry static

--- Utilities/ApiRegistry.cs
+++ Utilities/ApiRegistry.cs
@@ -4,37 +4,50 @@ using System.Collections.Generic;
 
 namespace Common.Utilities
 {
-    public class ApiRegistry(IModHelper helper, IMonitor monitor)
+    public static class ApiRegistry
     {
-        private readonly Dictionary<Type, object> _apis = [];
+        private static readonly Dictionary<Type, object> _apisDictionary = [];
+        private static IModHelper? _helper;
+        private static IMonitor? _monitor;
+
+        internal static void Init(IModHelper helper, IMonitor monitor)
+        {
+            _helper = helper;
+            _monitor = monitor;
+        }
 
         /// <summary>
         /// Retrieves an instance of the specified API type from the mod registry.
         /// </summary>
-        public T? GetApi<T>(string apiName, bool logError = true) where T : class
+        public static T? GetApi<T>(string apiName, bool logError = true) where T : class
         {
+            if (_helper == null || _monitor == null)
+            {
+                throw new InvalidOperationException("ApiRegistry is not initialized.");
+            }
+
             try
             {
-                if (_apis.TryGetValue(typeof(T), out var api) && api is T typedApi)
+                if (_apisDictionary.TryGetValue(typeof(T), out var api) && api is T typedApi)
                 {
                     return typedApi;
                 }
 
-                api = helper.ModRegistry.GetApi<T>(apiName);
+                api = _helper.ModRegistry.GetApi<T>(apiName);
 
                 if (api == null)
                 {
-                    monitor.Log($"Failed to hook into {apiName}.", logError ? LogLevel.Error : LogLevel.Trace);
+                    _monitor.Log($"Failed to hook into {apiName}.", logError ? LogLevel.Error : LogLevel.Trace);
                     return null;
                 }
 
-                _apis[typeof(T)] = api;
-                monitor.Log($"Successfully hooked into {apiName}.");
+                _apisDictionary[typeof(T)] = api;
+                _monitor.Log($"Successfully hooked into {apiName}.");
                 return (T)api;
             }
             catch (Exception e)
             {
-                monitor.Log($"Failed to hook into the {apiName} API. Please check if the mod has an update: {e.Message}", LogLevel.Warn);
+                _monitor.Log($"Failed to hook into the {apiName} API. Please check if the mod has an update: {e.Message}", LogLevel.Warn);
                 return null;
             }
         }
