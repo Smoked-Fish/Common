39a9b867d51c70658f99ee0c4eec8e062d73b636
Smoked Fish <espy.jacob@gmail.com>
2024-05-28 23:59:04 -0700

    removed unused

--- Utilities/Exceptions/PatchingException.cs
+++ Utilities/Exceptions/PatchingException.cs
@@ -4,26 +4,13 @@ namespace Common.Utilities.Exceptions
 {
     public class PatchingException : Exception
     {
-        public PatchingException()
-        {
-        }
+        public PatchingException() { }
 
-        public PatchingException(string message) : base(message)
-        {
-        }
+        public PatchingException(string message) : base(message) { }
 
-        public PatchingException(string message, Exception innerException) : base(message, innerException)
-        {
-        }
+        private PatchingException(string message, Exception innerException) : base(message, innerException) { }
 
         public PatchingException(string originalMethod, string newMethod, Exception innerException)
-            : this($"An error occurred while patching {originalMethod} with {newMethod}. See inner exception for details.", innerException)
-        {
-            OriginalMethod = originalMethod;
-            NewMethod = newMethod;
-        }
-
-        public string? OriginalMethod { get; }
-        public string? NewMethod { get; }
+            : this($"An error occurred while patching {originalMethod} with {newMethod}. See inner exception for details.", innerException) { }
     }
 }
